cc3060e71f748fd4198a7fbee939c5eddc6ef646
Ananya Dwivedi <171579240+Ananya-Dwivedi@users.noreply.github.com>
2026-07-17 18:17:48 +0530

    Time: 0 ms (100%), Space: 13.5 MB (34.84%) - LeetHub - Time: O(n), Space: O(n)

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,11 +1,22 @@
 class Solution(object):
     def singleNumber(self, nums):
-        freq={}
-        for n in nums:
-            freq[n]=freq.get(n,0)+1
-        for key in freq:
-            if freq[key]==1:
-                return key
+        # freq={}
+        # for n in nums:
+        #     freq[n]=freq.get(n,0)+1
+        # for key in freq:
+        #     if freq[key]==1:
+        #         return key
+
+        unique_nums=set()
+        for num in nums:
+            unique_nums.add(num)
+        single=(3*sum(unique_nums) -sum(nums))//2
+        return single
+
+
+
+
+            
         
 
 # Synced seamlessly with LeetHub Pro
