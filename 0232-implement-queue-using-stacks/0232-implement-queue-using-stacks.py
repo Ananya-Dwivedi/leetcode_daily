172dfd45628f077dc279287ff689ea64901741b7
Ananya Dwivedi <171579240+Ananya-Dwivedi@users.noreply.github.com>
2026-07-30 17:34:46 +0530

    Time: 0 ms (100%), Space: 12.4 MB (64.3%) - LeetHub - Time: O(1), Space: O(n)

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -1,28 +1,63 @@
+# from collections import deque
 class MyQueue(object):
 
     def __init__(self):
-        self.my_queue=[]
+        self.input_stack=[]
+        self.output_stack=[]
+        # self.my_queue=deque()
 
     def push(self, x):
         
-            self.my_queue.append(x)
+            self.input_stack.append(x)
+     
+            # self.my_queue.append(x)
     
 
     def pop(self):
-        if not self.my_queue:
-            return null
-        popped_element= self.my_queue[0]
-        for i in range(1,len(self.my_queue)):
-            self.my_queue[i-1]=self.my_queue[i]
-        return popped_element
+
+        if not self.output_stack:
+            
+            while self.input_stack:
+                popped_element=self.input_stack.pop()
+                self.output_stack.append(popped_element)
+            return  self.output_stack.pop()
+        
+        else:
+            return  self.output_stack.pop()
+
+
+        # if not self.my_queue:
+        #     return None
+        # return self.my_queue.popleft()
+        # popped_element= self.my_queue[0]
+        # for i in range(1,len(self.my_queue)):
+        #     self.my_queue[i-1]=self.my_queue[i]
+        # self.my_queue.pop()
+        
+        # return popped_element
 
       
     def peek(self):
-        return self.my_queue[0]
+        if not self.output_stack:
+            
+            while self.input_stack:
+
+                self.output_stack.append(self.input_stack.pop())
+            return  self.output_stack[-1]
+        
+        else:
+            return  self.output_stack[-1]
+
+        # return self.my_queue[0]
         
     def empty(self):
-        if  self.my_queue:
+        if not self.input_stack and not self.output_stack:
+            return True
+        else:
             return False
+        # if  self.my_queue:
+        #     return False
+        # return True
                 
 
 
