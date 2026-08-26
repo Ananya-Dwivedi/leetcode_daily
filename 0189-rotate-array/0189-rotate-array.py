49ed9c6c8e33ce9a2a0acde29a8c5fa6917a2b7e
Ananya Dwivedi <171579240+Ananya-Dwivedi@users.noreply.github.com>
2026-08-26 16:49:42 +0530

    Time: 182 ms (5.32%), Space: 26.9 MB (16.01%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        k=k%len(nums)
+        def reverse(left,right):
+            while left < right:
+                nums[left],nums[right]=nums[right],nums[left]
+                left+=1
+                right-=1
+        reverse(0,len(nums)-1)
+        reverse(0,k-1)
+        reverse(k,len(nums)-1)
+
+       
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
